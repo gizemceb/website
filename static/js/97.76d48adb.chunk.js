befa8d49ddbcf2fca4da02201183357c2f6ad884
Gizem Çebi <gizem.cebi@yolda.com>
2023-01-13 22:25:50 +0300

    Updates

---
+++ static/js/97.76d48adb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkwebsite=self.webpackChunkwebsite||[]).push([[97],{2097:function(e,u,n){n.r(u),n.d(u,{default:function(){return r}});var t=n(4190),s=n(184);function r(){return(0,s.jsx)(t.xu,{})}}}]);
+//# sourceMappingURL=97.76d48adb.chunk.js.map
